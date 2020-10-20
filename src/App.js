73d66f2607405e3541f4fe55104867b40b1bfe43
Kim Rasmussen <kim@Kims-MacBook-Pro.local>
2020-10-20 12:04:46 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,25 +5,96 @@ import Display from "./Display";
 import Form from "./Form";
 
 function App() {
+  // URL VARIABLE
+  const url = "https://dogsbackend14.herokuapp.com"
+  // STATE TO HOLD DOGS
+  const [dogs, setDogs] = React.useState([])
+  // Empty dog object to pass to the form so the form's initial state is blank
+  const emptyDog = {
+    name: "",
+    age: 0,
+    img: ""
+  }
+
+  //SelectDog for user to select a dog to update
+  const [selectedDog, setSelectedDog] = React.useState(emptyDog)
+
+  //handleCreate Function for creating dogs
+  //fetch by default is a GET request
+  //data always goes into the body. Passing in the dog data which is a JS object and then need to convert to json string
+  const handleCreate = (newDog) => {
+    fetch(url+"/dog/", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newDog)
+    })
+    .then(response => getDogs())
+  }
+
+  //handleUpdate to update a dog when form is clicked
+  const handleUpdate = (dog) => {
+    //dog being passed in is an object with the _id key
+    fetch(url + "/dog/" + dog._id, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dog)
+      })
+      .then(response => getDogs())
+    }
+
+    //selectDog which selects a dog
+    const selectDog = (dog) => {
+      setSelectedDog(dog)
+    }
+    //deleteDog function to delete a dog
+    //we are only sending the ID of the dog in the url and aren't sending any json data over to server so don't need to pass headers/body.
+    const deleteDog = (dog) => {
+      fetch(url + "/dog/" + dog._id, {
+        method: "delete"
+      })
+      .then(response => getDogs())
+    }
+
+  // FUNCTION TO FETCH DOGS
+  const getDogs = () => {
+    fetch(url + "/dog/")
+    .then(response => response.json())
+    .then(data => {
+      setDogs(data)
+    })
+  }
+  //Get dogs on page load
+  React.useEffect(() => {
+    getDogs()
+  }, [])
+
   return (
     <div className="App">
       <h1>DOG LISTING SITE</h1>
       <hr />
+      <Link to="/create">
+        <button>Add Dog</button>
+      </Link>
       <main>
         <Switch>
-          <Route exact path="/" render={(rp) => <Display {...rp} />} />
+  {/* only way to pass router props {...rp} and any props you want, need to use render */}
+          <Route exact path="/" render={(rp) => <Display {...rp} dogs={dogs} selectDog={selectDog} deleteDog={deleteDog}/>} />
           <Route
             exact
             path="/create"
             render={(rp) => (
-              <Form {...rp} label="create" dog={{}} handleSubmit={() => {}} />
+              <Form {...rp} label="create" dog={emptyDog} handleSubmit={handleCreate} />
             )}
           />
           <Route
             exact
             path="/edit"
             render={(rp) => (
-              <Form {...rp} label="update" dog={{}} handleSubmit={() => {}} />
+              <Form {...rp} label="update" dog={selectedDog} handleSubmit={handleUpdate} />
             )}
           />
         </Switch>
--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Display = (props) => {
-  return <h1>Display</h1>;
+  console.log('props', props)
+
+  //you want this as a function instead of just a variable
+  //so that you can call it when you want to
+  const {dogs} = props
+  console.log('dogs', {dogs})
+
+  //wrapped the jsx in a loaded function so that it will render only once it has the data, otherwise returns 'Loading...' message
+  const loaded = () => (
+    <div style={{textAlign: "center"}}>
+      {dogs.map(dog => (
+        <article>
+          <img src={dog.img}/>
+          <h1>{dog.name}</h1>
+          <h3>{dog.age}</h3>
+          <button onClick={() => {
+            props.selectDog(dog)
+            props.history.push("/edit")
+          }}>Edit</button>
+          <button onClick={() => {
+            props.deleteDog(dog)
+          }}>Delete</button>
+        </article>
+      ))}
+      </div>
+  )
+  return dogs.length > 0 ? loaded():
+  <h1>Loading....</h1>;
 };
 
 export default Display;
